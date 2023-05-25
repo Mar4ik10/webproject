d69312399d3f0358f71834211b998ba6add5e9bb
Mar4ik10 <markflint2608@gmail.com>
2023-05-25 23:37:25 +0200

    Added html pages

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 from . import models
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['public'],
+        'DIRS': ['public', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
